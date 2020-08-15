6fd9de01adbec1b43c5843308efa6cb9038d6dc6
Cargill Seiveright <microcargill@hotmail.com>
2020-08-15 15:12:57 -0500

    Add navigation bar

--- src/index.js
+++ src/index.js
@@ -5,14 +5,35 @@ import './index.css';
 class MainPage extends React.Component {
   render() {
     return (
-      <div>
-        Hello World
+      <NavigationBar/>
+    );
+  };
+};
+
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <div className="navi-bar" >
+        <NavItem button={"Home"}/>
+        <NavItem button={"Users"}/>
+        <NavItem button={"Agents"}/>
+        <NavItem button={"Login/Logout"}/>
       </div>
     );
   };
 };
 
+class NavItem extends React.Component {
+  render() {
+    return (
+      <button className="navi-item" >
+        {this.props.button}
+      </button>
+    );
+  };
+};
+
 ReactDOM.render(
-  <div>Hello World</div>,
+  <MainPage/>,
   document.getElementById('root')
 );
