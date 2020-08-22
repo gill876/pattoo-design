3625ae9a823c0f1c59073c40df1e8e9313d8212f
Cargill Seiveright <microcargill@hotmail.com>
2020-08-22 15:08:47 -0500

    Add basic comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+/*Import essential React components*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*Import routing components*/
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+/*Import custom components*/
 import Home from './Home';
 
+/*Import stylesheet*/
 import './index.css';
 
+/*Where components are rendered*/
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
