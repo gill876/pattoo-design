ec08628df2ba94e01264f6fad7e276d6b15c2036
Cargill Seiveright <microcargill@hotmail.com>
2020-08-23 03:07:42 -0500

    Add delete functionality

--- src/Users.js
+++ src/Users.js
@@ -51,17 +51,37 @@ class UsersRow extends React.Component {
     constructor(props){
       super (props);
       this.state = {
-        enabled: false
+        enabled: false,
+        deleteButton: 'cursor-pointer',
+        deleteRow: ''
       }
   
       this.handleChange = this.handleChange.bind(this);
+      this.handleClick = this.handleClick.bind(this);
     };
   
     handleChange(event) {
-      this.setState({
-        enabled: !this.state.enabled
-      });
+        const target = event.target;
+        alert(this.state.enabled);
+        if (target.name === 'toggle'){
+            this.setState({
+                enabled: !this.state.enabled
+            });
+        }
     };
+
+    handleClick(event) {
+        const target = event.target;
+        if (target.className === 'cursor-pointer') {
+            const confirm = prompt("Enter \"OK\" to permanently delete user", "OK");
+            (confirm === "OK")? 
+            this.setState({
+                deleteButton: 'cursor-pointer noclick-btn',
+                deleteRow: 'line-through noclick-btn',
+                enabled: false
+            }) : alert("Operation cancelled")
+        }
+    }
   
     render() {
       const idx_user = this.props.user.idx_user;
@@ -71,7 +91,7 @@ class UsersRow extends React.Component {
       const user_type = this.props.user.user_type;
       return (
         <tbody className="text-gray-700">
-          <tr>
+          <tr className={this.state.deleteRow}>
             <td className="text-left py-3 px-4">{idx_user}</td>
             <td className="w-1/3 text-left py-3 px-4">{first_name}</td>
             <td className="w-1/3 text-left py-3 px-4">{last_name}</td>
@@ -87,7 +107,10 @@ class UsersRow extends React.Component {
             </td>
             <td className="text-left py-3 px-4">{user_type}</td>
             <td className="text-center py-3 px-4">
-                <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className="user-remove w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7a4 4 0 11-8 0 4 4 0 018 0zM9 14a6 6 0 00-6 6v1h12v-1a6 6 0 00-6-6zM21 12h-6" /></svg>
+                <div className={this.state.deleteButton}
+                    onClick={this.handleClick}>
+                    <svg fill="none" viewBox="0 0 24 24" stroke="red" className="user-remove w-6 h-6 noclick-btn"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7a4 4 0 11-8 0 4 4 0 018 0zM9 14a6 6 0 00-6 6v1h12v-1a6 6 0 00-6-6zM21 12h-6" /></svg>
+                </div>
             </td>
           </tr>
         </tbody>
