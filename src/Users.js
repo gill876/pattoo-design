590951f65923735e34f1fcf290accfd3e45b8d7f
Cargill Seiveright <microcargill@hotmail.com>
2020-08-23 01:16:09 -0500

    Change html class to JSX className

--- src/Users.js
+++ src/Users.js
@@ -8,66 +8,66 @@ class Users extends React.Component {
             <div className="grid md:grid-cols-4">
                 <Navigation/>
                 <div className="md:col-span-3 md:flex md:justify-center">
-                    <div class="md:px-32 py-8 w-full">
-                        <div class="shadow overflow-y-scroll rounded border-b border-gray-200">
-                            <table class="min-w-full bg-white">
-                                <thead class="bg-gray-800 text-white">
+                    <div className="md:px-32 py-8 w-full">
+                        <div className="shadow overflow-y-scroll rounded border-b border-gray-200">
+                            <table className="min-w-full bg-white">
+                                <thead className="bg-gray-800 text-white">
                                     <tr>
-                                    <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Name</th>
-                                    <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Last name</th>
-                                    <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Toggle</th>
-                                    <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Phone</th>
-                                    <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Email</th>
+                                    <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Name</th>
+                                    <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Last name</th>
+                                    <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Toggle</th>
+                                    <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Phone</th>
+                                    <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Email</th>
                                     </tr>
                                 </thead>
-                                <tbody class="text-gray-700">
+                                <tbody className="text-gray-700">
                                 <tr>
-                                    <td class="w-1/3 text-left py-3 px-4">Lian</td>
-                                    <td class="w-1/3 text-left py-3 px-4">Smith</td>
+                                    <td className="w-1/3 text-left py-3 px-4">Lian</td>
+                                    <td className="w-1/3 text-left py-3 px-4">Smith</td>
                                     <td>
-                                        <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                                        <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
                                             <input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer focus:outline-none"/>
                                             <label for="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
                                         </div>
                                     </td>
-                                    <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662">622322662</a></td>
-                                    <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+                                    <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+                                    <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
                                 </tr>
-                                <tr class="bg-gray-100">
-                                    <td class="w-1/3 text-left py-3 px-4">Emma</td>
-                                    <td class="w-1/3 text-left py-3 px-4">Johnson</td>
+                                <tr className="bg-gray-100">
+                                    <td className="w-1/3 text-left py-3 px-4">Emma</td>
+                                    <td className="w-1/3 text-left py-3 px-4">Johnson</td>
                                     <td>
-                                        <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                                        <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
                                             <input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer focus:outline-none"/>
                                             <label for="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
                                         </div>
                                     </td>
-                                    <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662">622322662</a></td>
-                                    <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+                                    <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+                                    <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
                                 </tr>
                                 <tr>
-                                    <td class="w-1/3 text-left py-3 px-4">Oliver</td>
-                                    <td class="w-1/3 text-left py-3 px-4">Williams</td>
+                                    <td className="w-1/3 text-left py-3 px-4">Oliver</td>
+                                    <td className="w-1/3 text-left py-3 px-4">Williams</td>
                                     <td>
-                                        <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                                        <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
                                             <input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer focus:outline-none"/>
                                             <label for="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
                                         </div>
                                     </td>
-                                    <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662">622322662</a></td>
-                                    <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+                                    <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+                                    <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
                                 </tr>
-                                <tr class="bg-gray-100">
-                                    <td class="w-1/3 text-left py-3 px-4">Isabella</td>
-                                    <td class="w-1/3 text-left py-3 px-4">Brown</td>
+                                <tr className="bg-gray-100">
+                                    <td className="w-1/3 text-left py-3 px-4">Isabella</td>
+                                    <td className="w-1/3 text-left py-3 px-4">Brown</td>
                                     <td>
-                                        <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                                        <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
                                             <input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer focus:outline-none"/>
                                             <label for="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
                                         </div>
                                     </td>
-                                    <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662">622322662</a></td>
-                                    <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+                                    <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+                                    <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
                                 </tr>
                                 </tbody>
                             </table>
