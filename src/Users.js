667bd3e0439f8ac3d0d1f0fd14cb01ddd7c2fea5
Cargill Seiveright <microcargill@hotmail.com>
2020-08-23 12:30:39 -0500

    Indent better

--- src/Users.js
+++ src/Users.js
@@ -4,29 +4,29 @@ import Navigation from './Navigation';
 
 class Users extends React.Component {
         render() {
-        const test_users = [{idx_user: 1,first_name: "Cargill",last_name: "Seiveright",username: "sivrite",user_type: "admin"},
-                            {idx_user: 2,first_name: "John",last_name: "Brown",username: "jbrown",user_type: "standard"},
-                            {idx_user: 3,first_name: "Mary",last_name: "Jane",username: "mjane",user_type: "standard"},
-                            {idx_user: 4,first_name: "Joy",last_name: "Campbell",username: "jcamp",user_type: "standard"}]
-        const user_rows = test_users.map((user)=>
-            <UsersRow key={user.idx_user} user={user}/>
-        );
-        return (
-            <div className="grid md:grid-cols-4">
-                <Navigation current={"users"}/>
-                <div className="md:col-span-3 md:flex md:flex-col md:justify-center">
-                    <div className="text-lg md:text-3xl text-center font-bold tracking-wider mt-3 pt-2">Manage Users</div>
-                    <div className="md:px-32 py-8 w-full">
-                        <div className="shadow overflow-y-scroll rounded border-b border-gray-200">
-                            <table className="min-w-full bg-white">
-                                <UsersHead/>
-                                {user_rows}
-                            </table>
+            const test_users = [{idx_user: 1,first_name: "Cargill",last_name: "Seiveright",username: "sivrite",user_type: "admin"},
+                                {idx_user: 2,first_name: "John",last_name: "Brown",username: "jbrown",user_type: "standard"},
+                                {idx_user: 3,first_name: "Mary",last_name: "Jane",username: "mjane",user_type: "standard"},
+                                {idx_user: 4,first_name: "Joy",last_name: "Campbell",username: "jcamp",user_type: "standard"}]
+            const user_rows = test_users.map((user)=>
+                <UsersRow key={user.idx_user} user={user}/>
+            );
+            return (
+                <div className="grid md:grid-cols-4">
+                    <Navigation current={"users"}/>
+                    <div className="md:col-span-3 md:flex md:flex-col md:justify-center">
+                        <div className="text-lg md:text-3xl text-center font-bold tracking-wider mt-3 pt-2">Manage Users</div>
+                        <div className="md:px-32 py-8 w-full">
+                            <div className="shadow overflow-y-scroll rounded border-b border-gray-200">
+                                <table className="min-w-full bg-white">
+                                    <UsersHead/>
+                                    {user_rows}
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
     }
 }
 
