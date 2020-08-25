1b897d0bbb1ec6baf7d12753ce079e0730bbae80
Cargill Seiveright <microcargill@hotmail.com>
2020-08-25 00:23:02 -0500

    Good enough fixing

--- src/PurgeData.js
+++ src/PurgeData.js
@@ -8,9 +8,11 @@ class PurgeData extends React.Component {
             <div className="flex flex-col md:grid md:grid-cols-7">
                 <Navigation current={"purge"}/>
                 <div className="md:col-span-6">
-                    <div className="text-lg md:text-3xl text-center font-bold tracking-wider mt-3 pt-2">Purge Data</div>
-                    <div className="md:px-10 text-center py-8 mx-5 bg-white shadow rounded border-b border-gray-200">
-                        Purge Data
+                    <div className="text-lg md:text-3xl text-center font-bold tracking-wider my-3 py-2">Purge Data</div>
+                    <div className="md:px-10 flex justify-center text-center py-8 mx-5 bg-white shadow rounded border-b border-gray-200">
+                        <div className="w-3/12 bg-blue-300">
+                            Purge Data
+                        </div>
                     </div>
                 </div>
             </div>
