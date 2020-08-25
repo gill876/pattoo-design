850d12bf7f64bc9ae3bd479c78f0c0c07644f8d6
Cargill Seiveright <microcargill@hotmail.com>
2020-08-25 00:11:06 -0500

    Good enough

--- src/PurgeData.js
+++ src/PurgeData.js
@@ -5,11 +5,11 @@ import Navigation from './Navigation';
 class PurgeData extends React.Component {
     render() {
         return (
-            <div className="grid md:grid-cols-7">
+            <div className="flex flex-col md:grid md:grid-cols-7">
                 <Navigation current={"purge"}/>
-                <div className="md:col-span-6 md:flex md:flex-col md:justify-center">
+                <div className="md:col-span-6">
                     <div className="text-lg md:text-3xl text-center font-bold tracking-wider mt-3 pt-2">Purge Data</div>
-                    <div className="md:px-10 py-8 mx-5 max-w-2xl w-auto">
+                    <div className="md:px-10 text-center py-8 mx-5 bg-white shadow rounded border-b border-gray-200">
                         Purge Data
                     </div>
                 </div>
