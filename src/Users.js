4007c5e8e217547053b299520599423350fd2e2c
Cargill Seiveright <microcargill@hotmail.com>
2020-08-25 07:54:07 -0500

    Fix toggle bug

--- src/Users.js
+++ src/Users.js
@@ -99,11 +99,23 @@ class UsersRow extends React.Component {
             <td className="text-left py-3 px-4">{username}</td>
             <td className="text-center py-3 px-4">
                 <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-600 ease-in">
-                    <input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer focus:outline-none"
+                    {/*<input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer focus:outline-none"
                         defaultChecked={this.state.enabled}
                         onChange={this.handleChange}>
                     </input>
-                    <label htmlFor="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+                    <label htmlFor="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>*/}
+
+                    <label className="switch">
+                        <input
+                            type="checkbox"
+                            name="toggle"
+                            id="toggle"
+                            defaultChecked={this.state.enabled}
+                            onChange={this.handleChange}
+                            >
+                        </input>
+                        <span className="slider round"></span>
+                    </label>
                 </div>
             </td>
             <td className="text-left py-3 px-4">{user_type}</td>
