202caf5e6d79b866a5559c3a2e721ae57d958691
Cargill Seiveright <microcargill@hotmail.com>
2020-08-26 13:29:21 -0500

    Fix Modal component

--- src/Datapoint.js
+++ src/Datapoint.js
@@ -7,7 +7,7 @@ class Datapoint extends React.Component {
             <DatapointRow key={datapoint.datapoint_name} datapoint={datapoint}/>
         );
         return (
-            <table className="w-auto bg-white overflow-y-scroll">
+            <table className="w-auto bg-white overflow-x-scroll">
                 <DatapointHead/>
                 {dataRows}
             </table>
@@ -19,13 +19,13 @@ class DatapointHead extends React.Component {
     render() {
         return (
             <thead className="bg-gray-600 text-white">
-            <tr>
-                <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Datapoint Name</th>
-                <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Polling Interval</th>
-                <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Enabled</th>
-                <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Created</th>
-                <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Modified</th>
-            </tr>
+                <tr>
+                    <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Datapoint Name</th>
+                    <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Polling Interval</th>
+                    <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Enabled</th>
+                    <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Created</th>
+                    <th className="text-left py-1 px-2 uppercase font-semibold text-sm">Modified</th>
+                </tr>
             </thead>
         );
     };
--- src/Modal.js
+++ src/Modal.js
@@ -15,12 +15,12 @@ class Modal extends React.Component {
                 <>
                     {/*Start*/}
                     <div 
-                        id="close-out"
+                        /*id="close-out"*/
                         style={this.props.mStyle.vIew}
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                        onClick={this.handleClick}
+                        /*onClick={this.handleClick}*/
                         >
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        <div className="relative w-auto my-6 mx-auto max-w-full md:max-w-3xl">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
@@ -40,7 +40,7 @@ class Modal extends React.Component {
                                     </button>
                                 </div>
                                 {/*body*/}
-                                <div className="relative p-6 flex-auto">
+                                <div className="relative p-6 flex-auto overflow-auto">
                                     {this.props.mElements.content}
                                 </div>
                                 {/*footer*/}
